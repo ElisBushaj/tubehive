273042a2ebf63d152d7fdb89c283df603b6a970c
elisbushaj <elisbushaj2@gmail.com>
2024-04-27 20:27:15 +0200

    Update Footer component in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,33 @@
 export default function Footer() {
-  return (
-    <footer className="flex flex-col text-white">
-      <div className="flex flex-wrap justify-between md:justify-normal px-4 py-6 sm:px-14 md:space-x-28 lg:space-x-32 bg-gradient-to-b from-black to-gray-950">
-        <div className="mb-5 md:mb-3 mr-6 md:mr-0 flex-col flex ">
-          <h3 className="text-lg font-bold mb-2">Tools</h3>
-          <div className="flex flex-col text-stone-300">
-            <a
-              className="hover:text-secondary duration-300 transition-colors"
-              href="/"
-            >
-              TubeHive - YouTube MP3 Downloader
-            </a>
-            <a
+	return (
+		<footer className="flex flex-col text-white">
+			<div className="flex flex-wrap justify-between md:justify-normal px-4 py-6 sm:px-14 md:space-x-28 lg:space-x-32 bg-gradient-to-b from-black to-gray-950">
+				<div className="mb-5 md:mb-3 mr-6 md:mr-0 flex-col flex ">
+					<h3 className="text-lg font-bold mb-2">Tools</h3>
+					<div className="flex flex-col text-stone-300">
+						<a
+							className="hover:text-secondary duration-300 transition-colors"
+							href="/"
+						>
+							TubeHive - YouTube MP3 Downloader
+						</a>
+						{/* <a
               className="hover:text-secondary duration-300 transition-colors"
               href="https://gramhive.netlify.app"
             >
               GramHive - Instagram Downloader
-            </a>
+            </a> */}
 
-            <a
-              className="hover:text-secondary duration-300 transition-colors"
-              href="https://tikhive.netlify.app/"
-            >
-              TikHive - Tiktok Video Downloader
-            </a>
-          </div>
-        </div>
+						<a
+							className="hover:text-secondary duration-300 transition-colors"
+							href="https://tikhive.netlify.app/"
+						>
+							TikHive - Tiktok Video Downloader
+						</a>
+					</div>
+				</div>
 
-        {/* <div className="mb-5 md:mb-3 mr-6 md:mr-0">
+				{/* <div className="mb-5 md:mb-3 mr-6 md:mr-0">
           <h3 className="text-lg font-bold mb-2">Legal</h3>
           <ul className="list-none text-stone-300">
             <li>
@@ -46,11 +46,11 @@ export default function Footer() {
             </li>
           </ul>
         </div> */}
-      </div>
-      <div className="flex flex-col items-center justify-center py-4 space-y-2 text-sm bg-gray-950">
-        <p>We are not affiliated with YouTube.</p>
-        <p>© 2023 TubeHive 1.0.2. All rights reserved.</p>
-      </div>
-    </footer>
-  );
+			</div>
+			<div className="flex flex-col items-center justify-center py-4 space-y-2 text-sm bg-gray-950">
+				<p>We are not affiliated with YouTube.</p>
+				<p>© 2023 TubeHive 1.0.2. All rights reserved.</p>
+			</div>
+		</footer>
+	);
 }
