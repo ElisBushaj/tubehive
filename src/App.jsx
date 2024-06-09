bdd992fd91c846828ec0f0a4cf2c106a0553f6d2
elisbushaj <elisbushaj2@gmail.com>
2024-06-09 20:20:22 +0200

    Added devtools detector that calls debugger if opened

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import Layout from "./components/Layout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage, SearchPage } from "./pages";
+import { useEffect } from "react";
 
 const App = () => {
+	useEffect(() => {
+		function _0x2757x1() {
+			const _0x2757x2 = /./;
+			_0x2757x2.toString = function () {
+				debugger;
+			};
+			console["log"]("%c", _0x2757x2);
+		}
+		const _0x2757x3 = setInterval(_0x2757x1, 0x3e8);
+
+		return () => {
+			clearInterval(_0x2757x3);
+		};
+	}, []);
+
 	return (
 		<BrowserRouter>
 			<Routes>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,13 +5,13 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
 	return (
-		<div className="w-screen flex flex-col">
+		<>
 			<Navbar />
 			<main className="min-h-[80vh] mt-24 w-full flex justify-center items-center">
 				<Outlet />
 			</main>
 			<Footer />
-		</div>
+		</>
 	);
 };
 
